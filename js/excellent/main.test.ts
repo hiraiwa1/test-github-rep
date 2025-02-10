142d4e0a0888211b96621016f285d5454fb6f695
hiraiwa1 <ichi.cun.sky.high@gmail.com>
2025-02-10 20:50:47 +0900

    delete test.yml

--- js/excellent/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { eventOrOdd } from "./main"
-
-describe('even or odd 関数', () => {
-  it('even パターン', () => {
-    expect(eventOrOdd(6)).toBe('odd');
-  })
-})
--- js/excellent/main.ts
+++
@@ -1,7 +0,0 @@
-export function eventOrOdd (number: number): string {
-  if(number % 2) {
-    return 'even'
-  } else {
-    return 'odd'
-  }
-}
